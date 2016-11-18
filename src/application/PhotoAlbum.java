cd6d4e20376b7cad36da0bb106522c3bde67589d
Tae <Tae@Tae-Tower>
2016-11-18 15:39:19 -0500

    fxml photo album

--- src/application/PhotoAlbum.java
+++ src/application/PhotoAlbum.java
@@ -1,13 +1,8 @@
 package application;
 	
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
-import utility.SerializeData;
-import view.LoginController;
-import view.NonAdminController;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
 
@@ -15,18 +10,11 @@ public class PhotoAlbum extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			SerializeData.initData();
-			FXMLLoader loader = new FXMLLoader();   
-			loader.setLocation(getClass().getResource("/view/LoginUI.fxml"));
-			AnchorPane root = (AnchorPane)loader.load();
-			LoginController loginController = loader.getController();
-			loginController.start(primaryStage);
-
-			Scene scene = new Scene(root, root.getPrefWidth(), root.getPrefHeight());
+			BorderPane root = new BorderPane();
+			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
-			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
