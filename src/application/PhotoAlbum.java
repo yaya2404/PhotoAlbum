565c471b82fb5d989e0bfb37a085476fb14d8ccb
Matthew Ya <matthew_ya@yahoo.com>
2016-11-18 17:35:27 -0500

    revert main PhotoAlbum class

--- src/application/PhotoAlbum.java
+++ src/application/PhotoAlbum.java
@@ -1,8 +1,12 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import utility.SerializeData;
+import view.LoginController;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
 
@@ -10,8 +14,14 @@ public class PhotoAlbum extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			SerializeData.initData();
+			FXMLLoader loader = new FXMLLoader();   
+			loader.setLocation(getClass().getResource("/view/LoginUI.fxml"));
+			AnchorPane root = (AnchorPane)loader.load();
+			LoginController loginController = loader.getController();
+			loginController.start(primaryStage);
+
+			Scene scene = new Scene(root, root.getPrefWidth(), root.getPrefHeight());
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
