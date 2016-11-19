f505aae0aaf1092fb2423f753705ce11a0e82773
Matthew Ya <matthew_ya@yahoo.com>
2016-11-18 19:30:21 -0500

    added some stuff to utility classes

--- src/utility/Photo.java
+++ src/utility/Photo.java
@@ -14,13 +14,28 @@ public class Photo implements Serializable{
 	private Date date;
 	private ArrayList<Tag> tags;
 	private Calendar calendar;
+	private String caption;
+	
 	public Photo(String path) {
 		// TODO Auto-generated constructor stub
-		calendar = new GregorianCalendar();
-		calendar.set(Calendar.MILLISECOND, 0);
-		image = new Image(path);
-		tags = new ArrayList<Tag>();
-		date = calendar.getTime();
+		this.calendar = new GregorianCalendar();
+		this.calendar.set(Calendar.MILLISECOND, 0);
+		this.image = new Image(path);
+		this.tags = new ArrayList<Tag>();
+		this.date = calendar.getTime();
+		this.caption = "";
 	}
-
+	public void setCaption(String text){
+		this.caption = text;
+	}
+	public void getInfo(){
+		StringBuilder mes = new StringBuilder();
+		mes.append("Caption: " + this.caption + "\n");
+		//append date
+		//mes.append("");
+		mes.append("Tags: \n" );
+		for(int i = 0; i < tags.size(); i++){
+			mes.append(tags.get(i).toString() + "\n");
+		}
+	}
 }
